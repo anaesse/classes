012fe72a1a799f2fe892e634e1616fc545876044
Sophia Edeki <sophiesse143@gmail.com>
2022-06-06 11:19:53 +0100

    updated

--- main.py
+++ main.py
@@ -3,10 +3,24 @@ from unicodedata import name
 
 class Student:
     # [assignment] Skeleton class. Add your code here
-    def __init__(self):
-        pass
+    def __init__(self, name, age, tracks, score):
+        self.name = name
+        self.age = age
+        self.tracks = tracks
+        self.score = score
 
+    def change_name(self, name):
+        self.name = name
 
+    def change_age(self, age):
+        self.age = age
+
+    def add_track(self, track):
+        self.tracks.append(track)
+
+    def get_score(self):
+        print(self.score)
+        
 
 Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
 
